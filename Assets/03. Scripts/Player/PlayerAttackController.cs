f4efa3a51696180155cdd917cf675fad3e97a59a
oneprize <bigsky010101@gmail.com>
2025-07-26 18:21:23 +0900

    0726_0619

--- Assets/03. Scripts/Player/PlayerAttackController.cs
+++ Assets/03. Scripts/Player/PlayerAttackController.cs
@@ -4,6 +4,7 @@ public class PlayerAttackController : MonoBehaviour
 {
     public Animator animator;                 // Slash 애니메이션 재생용
     public GameObject equippedWeapon;         // 현재 장착된 무기
+    public GameObject gunWeapon;
 
     void Update()
     {
@@ -12,6 +13,13 @@ public class PlayerAttackController : MonoBehaviour
         {
             animator.SetTrigger("Slash");    // Slash 트리거 실행
             Debug.Log("검 휘두르기!");
+        }
+
+        // 무기를 장착한 상태이고, 마우스 왼쪽 버튼을 눌렀다면
+        if (gunWeapon != null && Input.GetButtonDown("Fire1"))
+        {
+            animator.SetTrigger("Gunfire");    // Slash 트리거 실행
+            Debug.Log("총 !");
         }
     }
 }
